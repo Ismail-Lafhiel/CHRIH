21ca76dfb6fac5371971422648514df752399bb9
ismail-lafhiel <lafhiel.ismail.contact@gmail.com>
2024-02-16 16:16:00 +0100

    CHRIH-10 Created navbar and a carousel

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('home');
 
 Route::middleware([
     'auth:sanctum',

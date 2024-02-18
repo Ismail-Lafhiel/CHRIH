384c51b5005b4dfeafe34319872e5b5bcb347ec3
ismail-lafhiel <lafhiel.ismail.contact@gmail.com>
2024-02-18 13:38:10 +0100

    CHRIH-9 created cart page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,13 +29,12 @@
 
 <body class="font-sans antialiased">
     <x-banner />
-
     <div class="min-h-screen bg-white dark:bg-gray-900">
         {{-- @livewire('navigation-menu') --}}
 
         <!-- Page Heading -->
         @if (isset($header))
-            <header class="bg-white shadow">
+            <header class="bg-white shadow dark:bg-gray-800">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>

5ca32be3881869aba34649bb5d43be7b75afc222
meed ghoufrane <baloncesmeed@gmail.com>
2024-02-22 14:47:39 +0100

    edit home view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -128,10 +128,11 @@
                                     </svg>
                                 </div>
                                 <input type="search" oninput="search()" id="default-search"
-                                    class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-red-600 hover:bg-red-700 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="Search " required />
-                                <button type="submit"
-                                    class="text-white absolute end-2.5 bottom-2.5 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Search</button>
+                                {{-- <button type="submit"
+                                    class="text-white absolute end-2.5 bottom-2.5 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Search
+                                </button> --}}
                             </div>
                             {{-- filter   --}}
                             <script>
